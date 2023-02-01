673fb60bb01b24d6f30585cf14b2413fb350aa28
cristian <cristiandk49@gmail.com>
2023-02-01 18:55:22 +0100

    Meme_v3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs').promises
 const core = require('@actions/core')
 const github = require('@actions/github')
 
@@ -17,8 +18,11 @@ const resultado_tests = core.getInput('resultado_tests');
 let texto_meme;
 if (resultado_tests) {
     texto_meme = frase_positiva;
+    fs.writeFile('./readme.md', texto_meme)
+
 } else {
     texto_meme = frase_negativa;
+    fs.writeFile('./readme.md', texto_meme)
 }
 console.log(texto_meme);
 
